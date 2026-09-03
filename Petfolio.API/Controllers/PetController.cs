25ee35ab869d2392b355cff31973a2f1d92c7e56
Gabriela Silva <vigabiesdivision@gmail.com>
2026-09-03 09:23:55 -0300

    first commit

---
+++ Petfolio.API/Controllers/PetController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+
+namespace Petfolio.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PetController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
+    public IActionResult Register([FromBody] ResponseRegisterPetJson request)
+    {
+        //business logic to get all pets
+        return Created();
+    }
+
+}
---
+++ Petfolio.Communication/Requests/RequestRegisterPetJson.cs
@@ -0,0 +1,11 @@
+﻿using Petfolio.Communication.Enums;
+using System.Data;
+
+namespace Petfolio.Communication.Requests;
+
+public class RequestRegisterPetJson
+{
+    public string Name { get; set; } = string.Empty;
+    public DateTime Birthday {  get; set; }
+    public PetType Type { get; set; }
+}
